64b6cce57ccb6e53050b96b8043c5e2375f3c7ee
alina <alina@alinas-mbp-6>
2021-04-01 07:54:11 -0600

    generated tables and figures for PB, AB, and AB with half bridges

--- compute_acceptability.m
+++ compute_acceptability.m
@@ -2,4 +2,4 @@ function A = compute_acceptability(X, P, w)
 
 %P(X<w) = 0;
 %A = sum(P)*100;
-A = sum(P(find(X>=w)))*100;
+A = sum(P(X>=w))*100;
--- n_same_system_series.m
+++ n_same_system_series.m
@@ -1,25 +1,32 @@
 function [X, P] = n_same_system_series(n, x1, p1)
-X = zeros(n, size(x1,2));
-P = zeros(size(X));
 
-for i = 1:n
-    X(i,:) = x1;
-    P(i,:) = p1;
+if n==1
+    X = x1;
+    P = p1;
+else
+    X = zeros(n, size(x1,2));
+    P = zeros(size(X));
+
+    for i = 1:n
+        X(i,:) = x1;
+        P(i,:) = p1;
+    end
+
+    for i = 1:n-1
+     if i==1
+         tempX = combvec(X(i,:), X(i+1,:));
+         tempP = combvec(P(i,:), P(i+1,:));
+     else
+         tempX = combvec(tempX, X(i+1,:));
+         tempP = combvec(tempP, P(i+1,:));
+     end
+     tempX = min(tempX,[],1);
+     tempP = prod(tempP, 1);
+     [tempX, tempP] = combine_like_terms(tempX,tempP);
+    end
+    X = tempX;
+    P = tempP;
 end
- 
-for i = 1:n-1
- if i==1
-     tempX = combvec(X(i,:), X(i+1,:));
-     tempP = combvec(P(i,:), P(i+1,:));
- else
-     tempX = combvec(tempX, X(i+1,:));
-     tempP = combvec(tempP, P(i+1,:));
- end
- tempX = min(tempX,[],1);
- tempP = prod(tempP, 1);
- [tempX, tempP] = combine_like_terms(tempX,tempP);
 end
-X = tempX;
-P = tempP;
 
     
